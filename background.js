66e918125aaf054bb8829c7822308c0ff7a42800
gzitei <gustavo.zitei@gmail.com>
2024-10-05 18:32:03 -0300

    adds new functionalities

--- background.js
+++ background.js
@@ -1,46 +1,76 @@
-let popupIsOpen = false;
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === "complete" &&
+    tab.url.includes("web.whatsapp.com")
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: () => {
+        const sidebar = document.querySelector("#side")?.parentElement;
+        if (sidebar) sidebar.style.display = "none";
 
-chrome.runtime.onConnect.addListener((port) => {
-  if (port.name === "popup") {
-    popupIsOpen = true;
-    port.onDisconnect.addListener(() => {
-      popupIsOpen = false;
-    });
-  }
-});
+        const secondarybar = document.querySelector("._aohf");
+        if (secondarybar) secondarybar.style.display = "none";
 
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "toggle") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (popupIsOpen) {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          function: () => {
-            const sidebar = document.querySelector("#side")?.parentElement;
-            if (sidebar) {
-              chrome.runtime.sendMessage({
-                toggleSidebarCommandPressed: true,
-              });
+        const main = document.querySelectorAll("[class='_aigv _aigz']")[1];
+        main.style.maxWidth = `calc(100vw - var(--navbar-width))`;
+
+        const side = document.querySelector("[class='_aigv _aigw']");
+        side.style.minWidth = "380px";
+
+        const toggleSideBar = () => {
+          if (sidebar) {
+            if (secondarybar.style.display != "none") {
+              secondarybar.style.display = "none";
             }
-          },
-        });
-      } else {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          function: () => {
-            const sidebar = document.querySelector("#side")?.parentElement;
-            if (sidebar) {
-              sidebar.style.display =
-                sidebar.style.display === "none" ? "" : "none";
-              const header = sidebar.parentElement?.querySelector("header");
-              if (header) {
-                header.style.display =
-                  header.style.display === "none" ? "" : "none";
+            sidebar.style.display =
+              sidebar.style.display === "none" ? "" : "none";
+          }
+        };
+
+        const toggleSecondarySideBar = () => {
+          if (secondarybar) {
+            if (sidebar.style.display != "none") {
+              sidebar.style.display = "none";
+            }
+            secondarybar.style.display =
+              secondarybar.style.display === "none" ? "" : "none";
+          }
+        };
+
+        const app = document.querySelector(".app-wrapper-web ._aigs");
+
+        if (app) {
+          Object.assign(app.style, {
+            top: "0px",
+            "max-width": "100%",
+            "max-height": "100%",
+            height: "100%",
+            width: "100%",
+          });
+        }
+
+        document.body.addEventListener("click", (e) => {
+          let target = e.target;
+          let label = e.target.getAttribute("data-icon");
+          while (target) {
+            let role = target.role;
+            if (role) {
+              if (role == "listitem") {
+                toggleSideBar();
+              } else if (role == "button") {
+                if (label == "chats-filled") {
+                  toggleSideBar();
+                } else {
+                  toggleSecondarySideBar();
+                }
+                break;
               }
             }
-          },
+            target = target.parentElement;
+          }
         });
-      }
+      },
     });
   }
 });
